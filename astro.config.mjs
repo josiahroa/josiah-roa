c90164c43291f1a80c084808391c06301797e5a6
Josiah Roa <siahroa25@gmail.com>
2024-02-26 19:05:24 -0700

    chore: project init

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [react(), mdx(), tailwind()]
+});
